27609d0eb84ce68f5d7251f6950e2b8ad862d214
Skunk-XCP <skunk-xcp@hotmail.fr>
2024-04-29 18:22:32 +0200

    fix carousel

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from "react";
-import "swiper/css"; // assure-toi que cette ligne est dans ton fichier principal ou spécifique de composant
+import React, { useEffect, useState } from "react";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import { EffectCoverflow, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SkillsData from "../../data/skills.json";
 import SkillCard from "../SkillCard/SkillCard";
@@ -7,26 +10,42 @@ import s from "./style.module.css";
 
 const Skills = () => {
    const [activeIndex, setActiveIndex] = useState(0);
+   const [currentText, setCurrentText] = useState("");
+
+   // Mettre à jour l'index actif et le texte correspondant
+   const handleSlideChange = (swiper) => {
+      const newIndex = swiper.realIndex;
+      setActiveIndex(newIndex);
+      setCurrentText(SkillsData[newIndex].text);
+   };
+
+   useEffect(() => {
+      // Mettre à jour le texte actuel lors du premier rendu
+      if (SkillsData.length > 0) {
+         setCurrentText(SkillsData[activeIndex].text);
+      }
+   }, [activeIndex]);
 
    return (
       <section className={s.section}>
          <h2>Skillset</h2>
          <div className={s.container}>
             <Swiper
-               spaceBetween={50}
-               slidesPerView={3}
-               centeredSlides={true}
-               loop={true}
+               effect={"coverflow"}
                grabCursor={true}
-               onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
-               breakpoints={{
-                  768: {
-                     slidesPerView: 1,
-                  },
-                  1024: {
-                     slidesPerView: 3,
-                  },
+               centeredSlides={true}
+               slidesPerView={"auto"}
+               loop={true}
+               onSlideChange={handleSlideChange}
+               coverflowEffect={{
+                  rotate: 50,
+                  stretch: 0,
+                  depth: 400,
+                  modifier: 1,
+                  slideShadows: true,
                }}
+               pagination={true}
+               modules={[EffectCoverflow, Pagination]}
             >
                {SkillsData.map((skill) => (
                   <SwiperSlide key={skill.id}>
@@ -36,9 +55,9 @@ const Skills = () => {
             </Swiper>
          </div>
 
-         {SkillsData.length > 0 && (
+         {currentText && (
             <div className={s.description}>
-               <p>{SkillsData[activeIndex].text}</p>
+               <p>{currentText}</p>
             </div>
          )}
       </section>
