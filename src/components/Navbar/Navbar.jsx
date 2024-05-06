64cfede448d53005c83ef13c4bed7326652476f7
Skunk-XCP <skunk-xcp@hotmail.fr>
2024-05-06 17:40:08 +0200

    Add burger menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./style.module.css";
 
 function Navbar() {
+   const [isActive, setIsActive] = useState(false);
+
+   const toggleMenu = () => {
+      setIsActive(!isActive);
+   };
+
+   // Fonction pour fermer le menu quand un lien est cliqué
+   const closeMenu = () => {
+      setIsActive(false);
+   };
+
    return (
       <nav className={s.navbar}>
-         <ul>
+         <ul className={isActive ? s.active : ""}>
             <li>
-               <a href="#home">Accueil</a>
+               <a href="#home" onClick={closeMenu}>
+                  Accueil
+               </a>
             </li>
             <li>
-               <a href="#skillset">SkillSet</a>
+               <a href="#skillset" onClick={closeMenu}>
+                  SkillSet
+               </a>
             </li>
             <li>none 1</li>
             <li>none 2</li>
          </ul>
+         <div
+            className={`${s.menu_burger} ${isActive ? s.active : ""}`}
+            onClick={toggleMenu}
+         >
+            <div></div>
+            <div></div>
+            <div></div>
+         </div>
       </nav>
    );
 }
