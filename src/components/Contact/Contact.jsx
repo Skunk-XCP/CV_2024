775afadd1c5763e9ebf5a3281ca900d79a94d21e
Skunk-XCP <skunk-xcp@hotmail.fr>
2024-05-13 15:23:21 +0200

    style on Contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,36 +1,83 @@
-import { motion } from "framer-motion";
-import React from "react";
+import { motion, useAnimation } from "framer-motion";
+import React, { useEffect, useRef } from "react";
 import logoX from "../../assets/logos/X_logo_2023.svg";
 import logoGithub from "../../assets/logos/github-mark-white.svg";
 import logoLinkedin from "../../assets/logos/linkedin.svg";
 import s from "./style.module.css";
 
 function Contact() {
+   const controls = useAnimation();
+   const rootRef = useRef();
+
    const socialMedia = [
-      { id: 1, name: "X", logo: logoX, url: "" },
+      { id: 1, name: "X", logo: logoX, url: "https://twitter.com/Donatien__R" },
       { id: 2, name: "Github", logo: logoGithub, url: "" },
-      { id: 3, name: "Linkedin", logo: logoLinkedin, url: "" },
+      {
+         id: 3,
+         name: "Linkedin",
+         logo: logoLinkedin,
+         url: "https://www.linkedin.com/in/donatien-rouzeirol-0ab070207/",
+      },
    ];
 
-   return (
-      <section id="contact" className={s.section}>
-         <h3 className={s.title}>Let's Connect</h3>
+   useEffect(() => {
+      const currentRef = rootRef.current;
+      const observer = new IntersectionObserver(
+         (entries) => {
+            // Pour chaque entrée, vérifie si l'élément est visible
+            entries.forEach((entry) => {
+               if (entry.isIntersecting) {
+                  // Anime les éléments seulement quand ils entrent dans le viewport
+                  controls.start((i) => ({
+                     scale: [0.3, 1],
+                     opacity: 1,
+                     transition: { duration: 0.5, delay: i * 0.2 },
+                  }));
+               }
+            });
+         },
+         { threshold: 0.5 } // Déclenche l'animation lorsque 50% de l'élément est visible
+      );
 
-         <a className={s.email} href="donatienr@hotmail.fr">
-            donatienr@hotmail.fr
-         </a>
+      if (rootRef.current) {
+         observer.observe(rootRef.current);
+      }
+
+      return () => {
+         // Nettoyage de l'observer
+         if (currentRef) {
+            observer.unobserve(currentRef);
+         }
+      };
+   }, [controls]);
+
+   return (
+      <section ref={rootRef} id="contact" className={s.section}>
+         <div className={s.title_container}>
+            <h3 className={s.title}>Let's Connect</h3>
+         </div>
+
+         <div className={s.mail_container}>
+            <a className={s.email} href="donatienr@hotmail.fr">
+               donatienr@hotmail.fr
+            </a>
+         </div>
 
          <div className={s.social_media_container}>
-            {socialMedia.map((media) => (
+            {socialMedia.map((media, index) => (
                <motion.div
                   key={media.id}
                   className={s.social_media_card}
                   initial={{ scale: 0, opacity: 0 }}
-                  animate={{ scale: [0.3, 1.1, 1], opacity: 1 }}
-                  transition={{ duration: 0.5, times: [0, 0.75, 1] }}
+                  custom={index}
+                  animate={controls}
                >
                   <a href={media.url} target="_blank" rel="noopener noreferrer">
-                     <img src={media.logo} alt={media.name} />
+                     <img
+                        src={media.logo}
+                        alt={media.name}
+                        className={s.social_media_logo}
+                     />
                   </a>
                </motion.div>
             ))}
